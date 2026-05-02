3f949e2c7c8295e86043a6b8c121e5171c15648b
Goncalo Faria <goncalofaria74@gmail.com>
2026-05-02 17:38:53 +0100

    volume

--- storage.py
+++ storage.py
@@ -2,14 +2,14 @@ from pathlib import Path
 import json
 import os
 
-DATA_PATH = Path(os.getenv("DATA_PATH", "/data.json"))
+DATA_PATH = Path(os.getenv("DATA_PATH", "data.json"))
 
 def load_data():
-    if not os.path.exists(DATA_PATH):
+    if not (DATA_PATH).exists():
         return {}
-    with open(DATA_PATH, "r", encoding="utf-8") as f:
-        return json.load(f)
+    
+    return json.loads(DATA_PATH.read_text(encoding="utf-8"))
 
 def save_data(data):
     DATA_PATH.parent.mkdir(parents=True, exist_ok=True)
-    DATA_PATH.write_text(json.dumps(data, indent=4), encoding="utf-8")
+    DATA_PATH.write_text(json.dumps(data, indent=4, ensure_ascii=False), encoding="utf-8")
